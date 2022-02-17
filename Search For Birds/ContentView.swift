09ed32abc6935ebc6f5b19b7f8c36f0761c8c664
Kevin Rendon <kevinrendon99@gmail.com>
2022-02-17 17:14:37 -0500

    Initial Commit

---
+++ Search For Birds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Search For Birds
+//
+//  Created by Kevin on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Search For Birds/Search_For_BirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_For_BirdsApp.swift
+//  Search For Birds
+//
+//  Created by Kevin on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Search_For_BirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
